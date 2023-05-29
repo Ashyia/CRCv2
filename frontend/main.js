d080d025d265d9ffb92cce76c1c5641464f96d6c
Jakub Michalek <riikimaru07@gmail.com>
2023-05-29 20:18:16 +0100

    Created CI/CD Workflows

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://crc-api-fin.azurewebsites.net/api/GetResumeCounter?code=f5dyDdB0lipPG_B9__4xZ3LCpPlwQUqEzzTRX-yjGHFyAzFuxO1wtQ==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
